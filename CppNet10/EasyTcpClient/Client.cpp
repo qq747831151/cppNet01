00684764f7b06acde5a476e4120684407f45292a
qq747831151 <747831151@qq.com>
2021-08-26 18:18:25 +0800

    客户端跨平台移植 支持 Windows linux Macos1.4版本

--- CppNet10/EasyTcpClient/Client.cpp
+++ CppNet10/EasyTcpClient/Client.cpp
@@ -6,13 +6,38 @@
 	功能：客户端
 *****************************************************/
 #pragma once
+#if _WIN32
+
 #define  WIN32_LEAN_AND_MEAN  //不影响 windows.h 和 WinSock2.h 前后顺序 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS //这个用于 inet_ntoa   可以在右击项目属性 C/C++ 预处理里面 预处理定义添加
 #include <WinSock2.h>
 #include<windows.h>
-#include <stdio.h>
 /*为了可以在其他平台也可以使用 右键项目属性 选择链接器 附加依赖项 将ws2_32.lib 添加进去就行 这样就不需要 下面这些 */
 #pragma  comment(lib,"ws2_32.lib")
+
+#else
+
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<sys/stat.h>
+#include<string.h>
+#include <sys/time.h>
+#include<pthread.h>
+#include <arpa/inet.h>
+#include<sys/wait.h>
+#include<sys/mman.h>
+#include <sys/select.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <errno.h>
+
+#endif
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#include <stdio.h>
 #include <thread>
 enum CMD
 {
@@ -86,7 +111,7 @@ int Processor(SOCKET clientSockt)
 {
 	char szRecv[4096] = {};//缓冲区
 		//5.接收客户端请求数据  数据存到szRecv中第三个参数可接收得最大长度 最后一个设置为0
-	int nlen = recv(clientSockt, szRecv, sizeof(DataHeader), 0);//返回值是接收的长度
+	int nlen =(int)recv(clientSockt, szRecv, sizeof(DataHeader), 0);//返回值是接收的长度  MAC修改的地方
 	if (nlen <= 0)
 	{
 		printf("客户端已退出,任务结束\n");
@@ -131,6 +156,7 @@ int Processor(SOCKET clientSockt)
 	}
 		   break;
 	}
+	return 1;
 }
 bool g_bExit = true;//线程退出
 //这个负责发送数据
@@ -168,10 +194,13 @@ void cmdThread(SOCKET sock)
 }
 int main()
 {
+#if _WIN32
 	/*启动socket网络环境 2.x环境*/
 	WORD ver = MAKEWORD(2, 2);//版本号
 	WSADATA dat;
 	WSAStartup(ver, &dat);
+#endif
+	
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock == INVALID_SOCKET)
 	{
@@ -185,7 +214,12 @@ int main()
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
+#if _WIN32
 	_sin.sin_addr.S_un.S_addr = inet_addr("192.168.17.1");
+#else
+	inet_pton(AF_INET, "192.168.17.1", &_sin.sin_addr.s_addr);
+#endif
+	
 	int ret = connect(sock, (sockaddr*)&_sin, sizeof(_sin));
 	if (ret == -1)
 	{
@@ -206,7 +240,7 @@ int main()
 		struct timeval time;
 		time.tv_sec = 0;
 		time.tv_usec = 0;
-		int ret= select(sock, &fd_read, NULL, NULL, &time);
+		int ret= select(sock+1, &fd_read, NULL, NULL, &time);
 		if (ret<0)
 		{
 			printf("select, 任务结束\n");
@@ -225,10 +259,15 @@ int main()
 		//printf("空余时间处理其他业务\n");
 	
 	}
+#if _WIN32
 	//7.关闭套接字
-	closesocket(sock);
+	closesocket(sock);//这个是在windows下面的关闭函数
 	//清除windows socket环境
 	WSACleanup();
+#else
+	close(sock);
+#endif
+	
 	printf("客户端已退出,任务结束\n");
 	getchar();
 	return 0;
